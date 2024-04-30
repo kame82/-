36def2454da63af6caffe86973f68ddced7aef8e
kame_mac <kametyaso@gmail.com>
2024-05-01 01:29:13 +0900

    modify

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,7 +1,9 @@
 const path = require("path");
+const postcssImport = require("/Users/kaoru/ruby_playground/deploy/myapp/node_modules/postcss-import");
+
 module.exports = {
   plugins: [
-    require("postcss-import"),
+    postcssImport,
     require("autoprefixer"),
     require("postcss-nested"),
     require("postcss-flexbugs-fixes"),
