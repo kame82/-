ad6e54df83f1ea47da04fe3c983ee21d7a8b42d7
kame_mac <kametyaso@gmail.com>
2024-04-30 15:15:42 +0900

    update

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
 module.exports = {
   plugins: [
-    require("postcss-import"),
+    // require("postcss-import"),
     require("tailwindcss"),
     require("autoprefixer"),
-    require("postcss-nested"),
-    require("postcss-flexbugs-fixes"),
+    // require("postcss-nested"),
+    // require("postcss-flexbugs-fixes"),
   ],
 };
