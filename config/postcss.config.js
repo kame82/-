648381e5ed1a8510ecbec9c0e5c8982a3869213b
kame_mac <kametyaso@gmail.com>
2024-04-30 14:19:30 +0900

    update

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   plugins: [
-    require(path.resolve(__dirname, "node_modules", "postcss-import")),
+    require(path.resolve(__dirname, "..", "node_modules", "postcss-import")),
     require("tailwindcss"),
     require("autoprefixer"),
     require("postcss-nested"),
